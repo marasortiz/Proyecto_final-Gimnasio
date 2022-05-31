1a8c8f1f3fdb7f046da1277b6373de05415a50e3
Mara <santamaria.ortiz.tamara@gmail.com>
2022-05-31 16:12:43 +0200

    Esquema inicial / Formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* TABS */
+import { MatTabsModule } from '@angular/material/tabs';
+
+/* ICONS */
+import { MatIconModule } from '@angular/material/icon';
+
+/* FORM */
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FormularioComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
